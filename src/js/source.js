0ddf36cc124a97c6e7319e92730f71f87c4c546d
Wea1Storm <itlogger01@gmail.com>
2020-05-12 06:49:34 +0300

    test
    test

---
+++ src/js/source.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    // $('.btn__container').css('display', 'flex');
+    // $('.btn__container').css('justify-content', 'space-around');
+
+    // // $("button").click(function (e) { 
+    // //     e.preventDefault();
+    // //     console.log("You are clicking me")
+    // // });
+
+    // $('.btn').on('click', function () {
+    //     $('.nav').fadeIn(1000);
+    // });
+    // $('.btn2').on('click', function () {
+    //     $('.nav').fadeOut(1000);
+    // });
+
+    $(".nav:odd").css("border-left", "1px solid rgb(122, 122, 122)");
+    $(".nav:odd").css("border-right", "1px solid rgb(122, 122, 122)");
+});
